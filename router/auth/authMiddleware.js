c167f64513956c5028848a86a02d5ee2b0bfcd5e
LUTFU <dolaylutfu@gmail.com>
2024-10-19 04:48:31 +0300

    updated project

--- router/auth/authMiddleware.js
+++ router/auth/authMiddleware.js
@@ -1,19 +1,21 @@
 const jwt = require('jsonwebtoken');
 
 const authMiddleware = (req, res, next) => {
-    const token = req.headers['authorization']?.split(' ')[1];
-  
-    if (!token) {
-      return res.status(401).json({ message: 'Yetkisiz erişim' });
-    }
-  
-    try {
-      const decoded = jwt.verify(token, process.env.JWT_SECRET);
-      req.user = decoded;
-      next();
-    } catch (error) {
-      return res.status(401).json({ message: 'Geçersiz veya süresi dolmuş token' });
-    }
-  };
-  
-  module.exports = authMiddleware;
+  const token = req.headers['authorization']?.split(' ')[1];
+
+  if (!token) {
+    return res.status(401).json({ message: 'Yetkisiz erişim' });
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded;
+    next();
+  } catch (error) {
+    return res
+      .status(401)
+      .json({ message: 'Geçersiz veya süresi dolmuş token' });
+  }
+};
+
+module.exports = authMiddleware;
--- router/auth/authRouter.js
+++ router/auth/authRouter.js
@@ -1,14 +1,14 @@
 const MainRouter = require('express').Router();
 const authMiddleware = require('./authMiddleware');
-const User = require('../../models/User'); 
+const User = require('../../models/User');
 
 MainRouter.get('/', authMiddleware, async (req, res) => {
   try {
-    const user = await User.findById(req.user.id); 
-    res.status(200).json(user); 
+    const user = await User.findById(req.user.id);
+    res.status(200).json(user);
   } catch (error) {
     res.status(500).json({ message: 'Sunucu hatası' });
   }
 });
 
-module.exports = MainRouter; 
+module.exports = MainRouter;
